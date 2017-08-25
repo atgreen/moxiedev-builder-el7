cd187825b791978b04c0cd25308ce22723937139
Anthony Green <green@moxielogic.com>
2017-08-25 13:40:09 -0400

    fixes

--- convert.sh
+++ convert.sh
@@ -20,7 +20,7 @@ for target in moxie-elf moxiebox moxie-rtems; do
   rm -rf `find . -type d -name moxielogic-${target}-gcc\*`
 
   alien -g -k -d moxielogic-${target}-gcc-[c]*rpm
-  perl -p -i -e 's/^Depends.*/Depends: moxielogic-${target}-gcc , moxielogic-${target}-gcc-libstdc++/' moxielogic-${target}-gcc*/debian/control
+  perl -p -i -e 's/^Depends.*/Depends: moxielogic-${target}-gcc , moxielogic-${target}-gcc-libstdc++' moxielogic-${target}-gcc*/debian/control
   perl -p -i -e 's/Maintainer.*/Maintainer: green@moxielogic.com/' moxielogic-${target}-gcc*/debian/control
   (cd moxielogic-${target}-gcc*/debian/..;
    ./debian/rules binary)
@@ -33,7 +33,7 @@ for target in moxie-elf moxiebox moxie-rtems; do
    ./debian/rules binary)
   rm -rf `find . -type d -name moxielogic-${target}-gcc\*`
 
-  for tools in binutils gdb; do
+  for tool in binutils gdb; do
     alien -g -k -d moxielogic-${target}-${tool}-[0-9]*rpm
     perl -p -i -e 's/^Depends.*/Depends:/' moxielogic-${target}-${tool}*/debian/control
     perl -p -i -e 's/Maintainer.*/Maintainer: green@moxielogic.com/' moxielogic-${target}-${tool}*/debian/control
